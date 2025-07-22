231a198c48c249cc818b3d104f8614ebc6460279
Hakim <iamtheazizul@gmail.com>
2025-07-22 14:47:34 -0400

    random commit

--- u6.py
+++ u6.py
@@ -1,18 +1,10 @@
-class SongNode:
-	def __init__(self, artist, song, next=None):
-		self.song = song
-        self.artist = artist
-		self.next = next
+import matplotlib.pyplot as plt
 
-# For testing
-def print_linked_list(node):
-    current = node
-    while current:
-        print(current.song, end=" -> " if current.next else "")
-        current = current.next
-    print()
-		
-top_hits_2010s = SongNode("Uptown Funk", SongNode("Party Rock Anthem", SongNode("Bad Romance")))
-# top_hits_2010s = SongNode()
-
-print_linked_list(top_hits_2010s)
+plt.figure(figsize=(10, 6))
+plt.plot(env.episode_history, env.reward_history, marker='o', linestyle='-', label="Cumulative Reward")
+plt.xlabel("Episode")
+plt.ylabel("Cumulative Reward")
+plt.title("RL Training (DQN): Cumulative Reward over Episodes")
+plt.legend()
+plt.grid(True)
+plt.show()
